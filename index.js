24ef5ec1bc9c65f8c483342aed099d5d1fa574c8
Joseph-Chege <mjchege23@gmail.com>
2024-04-01 12:26:35 +0300

    Created first class functions

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
-// Code your solution in this file!
+// Code your solution in this 
+
+const drivers = ['Antonia', 'Nuru', 'Amari', 'Mo'];
+
+function returnFirstTwoDrivers(drivers){
+    return [...drivers].slice(0, 2);
+
+}
+
+function returnLastTwoDrivers(){
+    return [...drivers].slice(2);
+
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+
+
+function createFareMultiplier(multiplier){
+
+    return function fareMultiplier(fare){
+        return (fare * multiplier);
+    } 
+}
+  
+
+ const fareDoubler = createFareMultiplier(2);
+ const fareTripler = createFareMultiplier(3);
+    
+
+
+
+function selectDifferentDrivers(drivers, selectingDrivers){
+    if (selectingDrivers === returnFirstTwoDrivers){
+        return returnFirstTwoDrivers(drivers);
+
+    }else if (selectingDrivers === returnLastTwoDrivers){
+        return returnLastTwoDrivers(drivers);
+
+    }
+}
